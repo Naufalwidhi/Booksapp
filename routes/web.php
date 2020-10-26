4a9973755169e2618a2a9f03235870af23ed7325
Naufal <naufalwidhi1@gmail.com>
2020-10-27 00:31:09 +0700

    BooksApp

---
+++ routes/web.php
@@ -0,0 +1,9 @@
+<?php
+
+$router->get('/', function () use ($router) {
+    return $router->app->version();
+});
+$router->get('/key', function () {
+    return str_random(32);
+});
+$router->get('/foo', 'BooksController@index');
